c143c687d743cbb5290e4f722353861a48e39d86
Vegim Iseini <e1329771@student.tuwien.ac.at>
2021-03-18 14:22:01 +0100

    andd things to moth of them from main

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,5 +1,10 @@
 public class FirstClass {
     public static void main(String[] args) {
+        
+        makeAnyChange();
         System.out.println("This is first class in G2");
+    }
+
+    private static void makeAnyChange() {
     }
 }
